5a0952344cf412ceff2c2a4a1dfb339511b21b9e
sumitpandey5559 <sumitpandey559@gmail.com>
2017-07-11 16:31:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def webhook():
                        attachment_link = messaging_event["message"]["attachments"][0]["payload"]["url"]
                        send_message(sender_id, "Attachment recieved, Ok!")
                        send_photo(sender_id)
-              
+                       send_message(sender_id, "%s" % (attachment_link))
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
 
