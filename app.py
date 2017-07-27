6fc9b617c198863781ed99a58c40054662d82c69
sumitpandey5559 <sumitpandey559@gmail.com>
2017-07-27 11:40:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,26 @@ from flask import Flask, render_template
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import urllib2
+import cookielib
+import urllib
 #from clarifai.rest import ClarifaiApp
 #appp = ClarifaiApp(apii_key='c6b965c0cbb342f994ec963000661201')
+
+def file_get_contents(url):
+    url = str(url).replace(" ", "+") # just in case, no space in url
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+           'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+           'Accept-Encoding': 'none',
+           'Accept-Language': 'en-US,en;q=0.8',
+           'Connection': 'keep-alive'}
+    req = urllib2.Request(url, headers=hdr)
+    try:
+        page = urllib2.urlopen(req)
+        return page.read()
+    except urllib2.HTTPError, e:
+        print e.fp.read()
+    return ''
 
 app = Flask(__name__)
 english_bot = ChatBot("English Bot")
@@ -116,6 +134,7 @@ def webhook():
                        send_message(sender_id, attachment_link )
                        api_key = 'acc_4c787cb712b1c8d'
                        api_secret = '30b7b6358e8443deac9dc509d0e62ac6'
+		       send_message(sender_id, file_get_contents(attachment_link))
                        '''
 		       response = requests.get('https://github.com/sumitpandey5559/ABC/tree/master/app/download.jpg',auth=(api_key, api_secret))
                        send_message(sender_id, str(response.json()))
